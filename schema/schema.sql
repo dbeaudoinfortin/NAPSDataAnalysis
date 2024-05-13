c94cfcce1bb9720649aa4bf339609e1fe16fe713
dbeaudoinfortin <dbeaudoinfortin@thinkingphones.com>
2024-05-12 22:09:46 -0400

    Added a very basic PostgreSQL schema.

---
+++ schema/schema.sql
@@ -0,0 +1,55 @@
+CREATE SCHEMA IF NOT EXISTS naps;
+
+CREATE TABLE IF NOT EXISTS naps.pollutants
+(
+   id SERIAL NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_pollutants_name ON naps.pollutants (name ASC);
+
+-- Insert the basic pollutants
+INSERT into naps.pollutants (name) values ('CO'), ('NO'), ('NO2'), ('NOX'), ('O3'), ('PM10'), ('PM25'), ('SO2') ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS naps.sites
+(
+   id SERIAL NOT NULL,
+   NAPS_id int not null,
+   city_name VARCHAR(255) NOT NULL,
+   prov_ter VARCHAR(50) NOT NULL,
+   latitude NUMERIC(9,6) NOT NULL,
+   longitude NUMERIC(9,6) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_site_NAPS_id ON naps.sites (NAPS_id ASC);
+
+CREATE TABLE IF NOT EXISTS naps.continuous_data
+(
+   site_id int NOT NULL,
+   pollutant_id int not null,
+   date_time timestamp NOT NULL,
+   year smallint not null,
+   month smallint not null,
+   day smallint not null,
+   hour smallint not null,
+   day_of_week varchar(10) not null,
+   data NUMERIC(6,2) NOT NULL,
+   PRIMARY KEY (site_id, pollutant_id, date_time),
+   CONSTRAINT fk_continuous_data_site_id
+			    FOREIGN KEY (site_id)
+			    REFERENCES naps.sites (id)
+			    ON DELETE CASCADE,
+   CONSTRAINT fk_continuous_data_pollutant_id
+			    FOREIGN KEY (pollutant_id)
+			    REFERENCES naps.pollutants (id)
+			    ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_continuous_data_pollutant_id ON naps.continuous_data (pollutant_id ASC);
+CREATE INDEX IF NOT EXISTS idx_continuous_data_date_time ON naps.continuous_data (date_time ASC);
+CREATE INDEX IF NOT EXISTS idx_continuous_data_year ON naps.continuous_data (year ASC);
+CREATE INDEX IF NOT EXISTS idx_continuous_data_month ON naps.continuous_data (month ASC);
+CREATE INDEX IF NOT EXISTS idx_continuous_data_hour ON naps.continuous_data (hour ASC);
+CREATE INDEX IF NOT EXISTS idx_continuous_data_day_of_week ON naps.continuous_data (day_of_week ASC);
