4020b7b0f992d1461574aa64e015f2b66d0a0f81
Jean Beauchamp <jean@vwdubb.com>
2024-07-05 08:53:42 -0400

    add support for dbu, dbp. dbh args

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-THREADS=${THREADS:-2}
+DOWNLOADER_ARGS="-p /raw -t ${NAPS_THREADS}"
+LOADER_ARGS="-p /raw -dbh ${NAPS_DB_HOST} -dbu ${POSTGRES_USER} -dbp ${POSTGRES_PASSWORD} -t ${NAPS_THREADS}"
 
 if [ ${1} = "downloader" ]; then
-    java -cp naps_data.jar com.dbf.naps.data.download.sites.NAPSSitesDownloader -p /raw -t $THREADS
-    java -cp naps_data.jar com.dbf.naps.data.download.continuous.NAPSContinuousDataDownloader -p /raw -t $THREADS
-    java -cp naps_data.jar com.dbf.naps.data.download.integrated.NAPSIntegratedDataDownloader -p /raw -t $THREADS
+    java -cp naps_data.jar com.dbf.naps.data.download.sites.NAPSSitesDownloader $DOWNLOADER_ARGS
+    java -cp naps_data.jar com.dbf.naps.data.download.continuous.NAPSContinuousDataDownloader $DOWNLOADER_ARGS
+    java -cp naps_data.jar com.dbf.naps.data.download.integrated.NAPSIntegratedDataDownloader $DOWNLOADER_ARGS
 elif [ ${1} = "loader" ]; then
-    java -cp naps_data.jar com.dbf.naps.data.loader.sites.NAPSSitesLoader -p /raw -t $THREADS
-    java -cp naps_data.jar com.dbf.naps.data.loader.continuous.NAPSContinuousDataLoader -p /raw -t $THREADS
-    java -cp naps_data.jar com.dbf.naps.data.loader.integrated.NAPSIntegratedDataLoader -p /raw -t $THREADS
+    java -cp naps_data.jar com.dbf.naps.data.loader.sites.NAPSSitesLoader $LOADER_ARGS
+    java -cp naps_data.jar com.dbf.naps.data.loader.continuous.NAPSContinuousDataLoader $LOADER_ARGS
+    java -cp naps_data.jar com.dbf.naps.data.loader.integrated.NAPSIntegratedDataLoader $LOADER_ARGS
 else
     exit 1
 fi
